969cd09ec0a405e53b9759eb5285f38dc71cc1a1
Ingvald <ingvald.orbeck@gmail.com>
2019-11-21 14:44:48 +0100

    Basic schema and queries with graphql

---
+++ graphql-basic/src/data.js
@@ -0,0 +1,73 @@
+export const users = [
+    {
+        id: "1",
+    name: "Orbeck",
+    email:"ingvald.orbeck@gmail.com"
+    },
+    {
+        id: "2",
+    name: "Olard",
+    email:"olard.lucie@gmail.com",
+    age: 27
+    },
+    {
+        id: "3",
+    name: "Doe",
+    email:"john.doe@gmail.com",
+    age: 29
+    }
+];
+
+export const posts = [
+    {
+        id: '10',
+        title: 'GraphQl',
+        body: 'Learn GraphQl with Andrew',
+        published: false,
+        author: "2"
+    },
+    {
+        id: '11',
+        title: 'NodeJs',
+        body: 'I love NodeJs',
+        published: true,
+        author: "2"
+    },
+    {
+        id: '12',
+        title: 'No Idea',
+        body: 'Plus dur est le combat, plus belle est la victoire',
+        published: true,
+        author: "3"
+    },
+
+]
+
+export const comments = [
+    {
+        id: '20',
+        text: ' Great training, thanks to Andrew',
+        author: '1',
+        post: '10'
+    },
+    {
+        id: '21',
+        text: 'Marseille > QSG',
+        author: '1',
+        post: '12'
+        
+    },
+    {
+        id:'22',
+        text: 'Coucou',
+        author: '2',
+        post: '11'
+    },
+    {
+        id:'23',
+        text: 'zagrragerao kpokgraeo g gkr',
+        author: '3',
+        post: '11'
+        
+    }
+]
--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,29 +1,115 @@
 import {GraphQLServer} from 'graphql-yoga';
+import {users,comments,posts} from './data';
 
 //Type definitions [schema]
 const typeDefs = `
     type Query {
-        hello: String!
+        me:User!,
+        users(query:String): [User!],
+        post: Post!,
+        posts(query: String): [Post!],
+        comments: [Comment!]!
+    }
+
+    type User {
+        id: ID!
         name: String!
-        location: String!
-        bio: String
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]
+        
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        post: Post! 
+        author: User!
+        
+
+
     }
 `
 
 //resolvers
 const resolvers = {
     Query: {
-        hello(){
-            return "My first query string";
+        me(){
+            return users[0];
         },
-        name(){
-            return "Ingvald"
+        users(parent,args,ctxt,info){
+            console.log(args.query);
+            if(!args.query){
+                return users;
+            }
+
+            return users.filter((user)=>{
+                return user.name.toLowerCase().includes(args.query.toLowerCase()) 
+            })
         },
-        location(){
-            return "Toulouse"
+        post(){
+            return posts[0]
         },
-        bio(){
-            return " Learn GraphQl"
+        posts(parent,args,ctxt,info){
+            if(!args.query){
+                return posts
+            }
+
+            return posts.filter((post)=>{
+                return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        comments(paren,args,ctxt,info){
+            return comments
+        }
+    },
+    Post: {
+        author(parent,args,ctxt,info){
+            return users.find((user)=> {    
+                return user.id === parent.author
+                })
+        },
+        comments(parent,args,ctxt,info){
+            return comments.filter((comment)=>{
+                console.log(comment.id,parent.comment)
+                return comment.post === parent.id
+            })
+        }
+    },
+    User: {
+        posts(parent,args,ctxt,info){
+            return posts.filter((post)=>{
+               return  post.author === parent.id
+            })
+        }
+        ,
+        comments(parent,args,ctxt,info){
+            return comments.filter((comment)=>{
+                return comment.author === parent.id 
+            })
+        }
+    }
+    ,
+    Comment: {
+        author(parent,args,ctxt,info){
+            return users.find((user)=>{
+                return user.id === parent.author
+            })
+        },
+        post(parent,args,ctxt,info){
+            return posts.find((post)=>{
+                return post.id === parent.post
+            })
         }
     }
     
